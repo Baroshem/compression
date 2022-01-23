6b2b313f0204a05be2873fcfbc7911355e7899d2
Baroshem <jakub.andrzejewski.dev@gmail.com>
2022-01-23 18:33:26 +0100

    feat: create module types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,23 +1,53 @@
-import { ComputedRef } from 'vue'
+import type { ZlibOptions, BrotliOptions } from 'zlib';
 
-export type LazyLoadOptions = {
-  selector?: string;
-  options?: {
-    rootMargin: string;
-    threshold: number;
-    enableAutoReload: boolean;
-    [key: string]: any;
-  };
-  [key: string]: any;
-};
+// vite-plugin-compression types
+export type CompressOptions = Partial<ZlibOptions> | Partial<BrotliOptions>;
+export interface CompressionOptions {
+  /**
+   * Log compressed files and their compression ratios.
+   * @default: true
+   */
+  verbose?: boolean;
+  /**
+   * Minimum file size before compression is used.
+   * @default 1025
+   */
+  threshold?: number;
+  /**
+   * Filter files that do not need to be compressed
+   * @default /\.(js|mjs|json|css|html)$/i
+   */
+  filter?: RegExp | ((file: string) => boolean);
 
-export type Observer = {
-  observe: Function;
-  observer: IntersectionObserver;
-  triggerLoad: Function;
-}
+  /**
+   * Whether to enable compression
+   * @default: false
+   */
+  disable?: boolean;
+  /**
+   * Compression algorithm
+   * @default gzip
+   */
+  algorithm?: 'gzip' | 'brotliCompress' | 'deflate' | 'deflateRaw';
 
-export type UseLazyLoad = {
-  init(): Observer;
-  observer: ComputedRef<Observer>;
+  /**
+   * File format after compression
+   * @default .gz
+   */
+  ext?: string;
+
+  /**
+   * Compression Options
+   */
+  compressionOptions?: CompressOptions;
+  /**
+   * Delete the corresponding source file after compressing the file
+   * @default: false
+   */
+  deleteOriginFile?: boolean;
+
+  /**
+   * success callback after completed
+   */
+  success?: () => void;
 }
