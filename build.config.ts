86f47859d9046758377907f97880dd68019c3e9b
Baroshem <jakub.andrzejewski.dev@gmail.com>
2022-01-23 18:32:48 +0100

    feat: define build.config.ts

--- build.config.ts
+++ build.config.ts
@@ -3,10 +3,8 @@ import { defineBuildConfig } from 'unbuild'
 export default defineBuildConfig({
   entries: [
     './src/module',
-    { input: 'src/plugins/', outDir: 'dist/plugins' },
-    { input: 'src/composables/', outDir: 'dist/composables' },
     { input: 'src/types/', outDir: 'dist/types' }
   ],
-  externals: ['@nuxt/kit', '@nuxt/schema', 'defu', 'pathe', 'vue'],
+  externals: ['@nuxt/kit', '@nuxt/schema', 'defu', 'pathe', 'vue', 'vite-plugin-compression'],
   declaration: true
 })
